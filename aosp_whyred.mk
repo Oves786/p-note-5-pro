274fa7a7b88dda020df898fb6267939010c2c3a1
DarkLord1731 <darklord1731@gmail.com>
2018-08-08 05:19:34 +0000

    aosp fyO
    Signed-off-by: DarkLord1731 <darklord1731@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -19,17 +19,14 @@ PRODUCT_RELEASE_NAME := whyred
 
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := aosp_whyred
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
